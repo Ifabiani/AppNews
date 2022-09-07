4b22fe63e922591ec5e7b8c7ccbe4ac039406bb6
Ifabiani <ignaciofabiani@gmail.com>
2022-09-07 18:24:44 +0200

    style

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -3,39 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            News Challenge
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+    <div className="container">
+        <Link className="navbar-brand" to="/">
+           NewsApp
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse " id="navbarNav">
-            <ul className="navbar-nav ">
-              <li className="nav-item">
-                <Link className="navbar-brand" to="/">
-                    News
-                </Link>
-              </li>
-              <li className="nav-item">
-              <Link className="navbar-brand" to="/archived">
-                    Archived News
-                </Link>
-              </li>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <Link to="/" className="nav-link">News</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/archived" className="nav-link">Archived News</Link>
+                </li>
             </ul>
-          </div>
         </div>
-      </div>
-    </nav>
+    </div>
+</nav>
   );
 }
